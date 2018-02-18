a1a9664bd50255893e2a713602fd7cddbdbd6ec4
Vitalii <smstur@gmail.com>
2018-02-18 11:34:54 +0200

    add notes

--- convert.sh
+++ convert.sh
@@ -6,13 +6,14 @@ NEW_FOLDER="$CURRENT_FOLDER/out"
 mkdir "$NEW_FOLDER"
 
 for i in *."$EXTENSION";
-  #do name=`echo $i | cut -d'.' -f1`;
-#  do name="${i%[*.*}.mp4";
-   #do name="${i%]*}";
-   do name="${i/Miraculous.Tales.of.Ladybug.and.Cat.Noir.[/}";
-   name="${name%]*}.$EXTENSION";
+  # delete extension
+  # do name=`echo $i | cut -d'.' -f1`;
+   
+  # leave only season and episode. For example: Some.name.[s01e01].other.info.mp4 => s01e01.mp4
+  do name="${i/Some.name.[/}";
+  name="${name%]*}.$EXTENSION";
 
   echo $name;
-#  ffmpeg -i "$i" "${name}.mov";
+  # delete first audio track
   ffmpeg -i "$i" -map 0:0 -map 0:2 -acodec copy -vcodec copy "$NEW_FOLDER/$name"
 done
