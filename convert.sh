2f44ba379daea25de6ab168f6229c9dfe5472238
vitalii <vitalii.bilyk@masterofcode.com>
2018-04-20 12:25:20 +0300

    do not change name

--- convert.sh
+++ convert.sh
@@ -7,21 +7,13 @@ NEW_FOLDER="$CURRENT_FOLDER/out"
 mkdir "$NEW_FOLDER"
 
 for i in *."$EXTENSION";
-  # delete extension
-  do name=`echo $i | cut -d'.' -f1`;
-
-  # leave only season and episode. For example: Some.name.[s01e01].other.info.mp4 => s01e01.mp4
-  # do name="${i/Some.name.[/}";
-
-  name="${name%]*}.$EXTENSION";
-
-  echo $name;
+do
   # delete first audio track
   if [ "$EXTENSION" == "mp4" ]
     then
-    ffmpeg -i "$i" -map 0:0 -map 0:"$AUDIO_TRACK" -acodec copy -vcodec copy "$NEW_FOLDER/$name"
+    ffmpeg -i "$i" -map 0:0 -map 0:"$AUDIO_TRACK" -acodec copy -vcodec copy "$NEW_FOLDER/$i"
   elif [ "$EXTENSION" == "mkv" ]
     then
-    mkvmerge -o "$NEW_FOLDER/$name" --atracks "$AUDIO_TRACK" "$i"
+    mkvmerge -o "$NEW_FOLDER/$i" --atracks "$AUDIO_TRACK" "$i"
   fi
 done
