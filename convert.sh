666b79de84d5de3984f37f6fe25cb5f32809361a
vitalii <vitalii.bilyk@masterofcode.com>
2018-04-20 10:25:48 +0300

    add mkv

--- convert.sh
+++ convert.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-EXTENSION="mp4"
+EXTENSION="mkv"
+AUDIO_TRACK=2
 CURRENT_FOLDER=`pwd`
 NEW_FOLDER="$CURRENT_FOLDER/out"
 mkdir "$NEW_FOLDER"
 
 for i in *."$EXTENSION";
   # delete extension
-  # do name=`echo $i | cut -d'.' -f1`;
-   
+  do name=`echo $i | cut -d'.' -f1`;
+
   # leave only season and episode. For example: Some.name.[s01e01].other.info.mp4 => s01e01.mp4
-  do name="${i/Some.name.[/}";
+  # do name="${i/Some.name.[/}";
+
   name="${name%]*}.$EXTENSION";
 
   echo $name;
   # delete first audio track
-  ffmpeg -i "$i" -map 0:0 -map 0:2 -acodec copy -vcodec copy "$NEW_FOLDER/$name"
+  if [ "$EXTENSION" == "mp4" ]
+    then
+    ffmpeg -i "$i" -map 0:0 -map 0:"$AUDIO_TRACK" -acodec copy -vcodec copy "$NEW_FOLDER/$name"
+  elif [ "$EXTENSION" == "mkv" ]
+    then
+    mkvmerge -o "$NEW_FOLDER/$name" --atracks "$AUDIO_TRACK" "$i"
+  fi
 done
