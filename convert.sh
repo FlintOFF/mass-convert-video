5bc44205757736543e5422fceaa469e7ef4c6657
vitalii <vitalii.bilyk@masterofcode.com>
2018-06-26 02:30:35 +0300

    added feature split file by time

--- convert.sh
+++ convert.sh
@@ -2,6 +2,8 @@
 
 EXTENSION="mkv"
 AUDIO_TRACK=2
+TIME_FROM="00:00:00" #default is 00:00:00 (hh:mm:ss)
+TIME_TO="" #defaul is empty (hh:mm:ss)
 CURRENT_FOLDER=`pwd`
 NEW_FOLDER="$CURRENT_FOLDER/out"
 mkdir "$NEW_FOLDER"
@@ -14,6 +16,6 @@ do
     ffmpeg -i "$i" -map 0:0 -map 0:"$AUDIO_TRACK" -acodec copy -vcodec copy "$NEW_FOLDER/$i"
   elif [ "$EXTENSION" == "mkv" ]
     then
-    mkvmerge -o "$NEW_FOLDER/$i" --atracks "$AUDIO_TRACK" "$i"
+    mkvmerge --split parts:"$TIME_FROM"-"$TIME_TO" -o "$NEW_FOLDER/$i" --atracks "$AUDIO_TRACK" "$i"
   fi
 done
